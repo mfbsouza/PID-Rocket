5f748dd641685c46fceb7a28a09a5eb78a622f9c
mfbsouza <mfbsouza.it@gmail.com>
2021-11-14 22:33:41 -0300

    minor conversion of spaces to tabs

--- pid.py
+++ pid.py
@@ -1,73 +1,73 @@
 class PID:
-    def __init__(self, kp, ki, kd, setpoint, out_min_value, out_max_value) -> None:
+	def __init__(self, kp, ki, kd, setpoint, out_min_value, out_max_value) -> None:
 
-        # initial values
-        self.Kp = kp
-        self.Ki = ki
-        self.Kd = kd
+		# initial values
+		self.Kp = kp
+		self.Ki = ki
+		self.Kd = kd
 
-        self.setpoint      = setpoint
-        self.out_min_value = out_min_value
-        self.out_max_value = out_max_value
+		self.setpoint	   = setpoint
+		self.out_min_value = out_min_value
+		self.out_max_value = out_max_value
 
-        # compute variables
-        self.i_error          = 0
-        self.output           = 0
-        self.prev_time        = 0
-        self.prev_error       = 0
-    
-    def update_gains(self, kp, ki, kd):
-        self.Kp = kp
-        self.Ki = ki
-        self.Kd = kd
-        
-    def update_setpoint(self, setpoint):
-        self.setpoint = setpoint
+		# compute variables
+		self.i_error		  = 0
+		self.output			  = 0
+		self.prev_time		  = 0
+		self.prev_error		  = 0
+	
+	def update_gains(self, kp, ki, kd):
+		self.Kp = kp
+		self.Ki = ki
+		self.Kd = kd
+		
+	def update_setpoint(self, setpoint):
+		self.setpoint = setpoint
 
-    def update_output_limits(self, min_value, max_value):
-        self.out_max_value = max_value
-        self.out_min_value = min_value
+	def update_output_limits(self, min_value, max_value):
+		self.out_max_value = max_value
+		self.out_min_value = min_value
 
-    def __map(self, value):
-        slope = (self.out_max_value - self.out_min_value)/(1.0 * self.setpoint)
-        mapped_value = (slope * value) + self.out_min_value
-        return mapped_value
-    
-    def compute(self, current_time, measurement):
+	def __map(self, value):
+		slope = (self.out_max_value - self.out_min_value)/(1.0 * self.setpoint)
+		mapped_value = (slope * value) + self.out_min_value
+		return mapped_value
+	
+	def compute(self, current_time, measurement):
 
-        # time variation since last compute
-        delta_time = current_time - self.prev_time
+		# time variation since last compute
+		delta_time = current_time - self.prev_time
 
-        # error signal
-        error = self.setpoint - measurement
+		# error signal
+		error = self.setpoint - measurement
 
-        # proportional error
-        p_error = self.Kp * error
+		# proportional error
+		p_error = self.Kp * error
 
-        # integral error
-        # TODO: implement better way to avoid integral wind-up
-        if error < 20 and error > -20:
-            self.i_error = self.i_error + (self.Ki * error)
-        else:
-            self.i_error = 0
+		# integral error
+		# TODO: implement better way to avoid integral wind-up
+		if error < 20 and error > -20:
+			self.i_error = self.i_error + (self.Ki * error)
+		else:
+			self.i_error = 0
 
-        # derivative error
-        # TODO: implment anti derivative kick
-        d_error = self.Kd * (error - self.prev_error)/delta_time
+		# derivative error
+		# TODO: implment anti derivative kick
+		d_error = self.Kd * (error - self.prev_error)/delta_time
 
-        self.output = p_error + d_error + self.i_error
-        self.output = self.__map(self.output)
+		self.output = p_error + d_error + self.i_error
+		self.output = self.__map(self.output)
 
-        # clamp the output
-        if self.output > self.out_max_value:
-            self.output = self.out_max_value
+		# clamp the output
+		if self.output > self.out_max_value:
+			self.output = self.out_max_value
 
-        elif self.output < self.out_min_value:
-            self.output = self.out_min_value
-        
-        # remember some variables for next time
-        self.prev_error = error
-        self.prev_time  = current_time
+		elif self.output < self.out_min_value:
+			self.output = self.out_min_value
+		
+		# remember some variables for next time
+		self.prev_error = error
+		self.prev_time	= current_time
 
-        return self.output
+		return self.output
 
--- rocket.py
+++ rocket.py
@@ -1,89 +1,89 @@
 class Rocket:
-    def __init__(self, mass) -> None:
-        
-        # intial values
-        self.y_pos      = 0
-        self.speed      = 0
-        self.accel      = 0
-        self.prev_time  = 0
+	def __init__(self, mass) -> None:
+		
+		# intial values
+		self.y_pos		= 0
+		self.speed		= 0
+		self.accel		= 0
+		self.prev_time	= 0
 
-        # Rocket's weight force
-        self.mass = mass
-        self.gravity = -9.8
-        self.weight_force = self.mass * self.gravity
-        
-        # Rocket's truster
-        self.trust_force = 0
-        self.max_trust_force = 5.5 * (-self.weight_force)
+		# Rocket's weight force
+		self.mass = mass
+		self.gravity = -9.8
+		self.weight_force = self.mass * self.gravity
+		
+		# Rocket's truster
+		self.trust_force = 0
+		self.max_trust_force = 5.5 * (-self.weight_force)
 
-    def get_height(self):
-        return self.y_pos
-    
-    def update(self, current_time, gas_pedal):
-        # calculating the time variation since last update
-        delta_time = current_time - self.prev_time
-        self.prev_time = current_time
+	def get_height(self):
+		return self.y_pos
+	
+	def update(self, current_time, gas_pedal):
+		# calculating the time variation since last update
+		delta_time = current_time - self.prev_time
+		self.prev_time = current_time
 
-        # calculating the trust force
-        self.trust_force = gas_pedal * self.max_trust_force
+		# calculating the trust force
+		self.trust_force = gas_pedal * self.max_trust_force
 
-        # calculating the resulting forces on the rocket and its acceleration
-        resulting_force = self.weight_force + self.trust_force
-        self.accel = resulting_force/self.mass
+		# calculating the resulting forces on the rocket and its acceleration
+		resulting_force = self.weight_force + self.trust_force
+		self.accel = resulting_force/self.mass
 
-        # calculating the current position and speed
-        new_y = self.y_pos + (self.speed * delta_time) + (0.5 * self.accel * delta_time**2)
-        self.speed = self.speed + (self.accel * delta_time)
+		# calculating the current position and speed
+		new_y = self.y_pos + (self.speed * delta_time) + (0.5 * self.accel * delta_time**2)
+		self.speed = self.speed + (self.accel * delta_time)
 
-        # floor
-        if new_y <= 0:
-            self.y_pos = 0
-            self.speed = 0
-            self.accel = 0
-        else:
-            self.y_pos = new_y
+		# floor
+		if new_y <= 0:
+			self.y_pos = 0
+			self.speed = 0
+			self.accel = 0
+		else:
+			self.y_pos = new_y
 
-        return [self.y_pos, self.speed, self.accel]
+		return [self.y_pos, self.speed, self.accel]
 
 def demo():
-    import matplotlib.pylab as plt
+	import matplotlib.pylab as plt
 
-    height_vals = []
-    speed_vals  = []
-    accel_vals  = []
-    x_vals      = []
-    time        = 0.0
-    pedal       = 1.0
+	height_vals = []
+	speed_vals	= []
+	accel_vals	= []
+	x_vals		= []
+	time		= 0.0
+	pedal		= 1.0
 
-    simulation_time = 40.0
-    rocket_mass     = 10
+	simulation_time = 40.0
+	rocket_mass		= 10
 
-    rocket = Rocket(rocket_mass)
+	rocket = Rocket(rocket_mass)
 
-    while (time <= simulation_time):
-        height, speed, accel = rocket.update(time, pedal)
+	while (time <= simulation_time):
+		height, speed, accel = rocket.update(time, pedal)
 
-        height_vals.append(height)
-        speed_vals.append(speed)
-        accel_vals.append(accel)
-        x_vals.append(time)
+		height_vals.append(height)
+		speed_vals.append(speed)
+		accel_vals.append(accel)
+		x_vals.append(time)
 
-        # a bit simulation tests
-        if (height >= 125):
-            pedal = 0.0
-        elif (time >= 21.0 and time < 22.0):
-            pedal = 1.0
-        time = time + 0.01
+		# a bit simulation tests
+		if (height >= 125):
+			pedal = 0.0
+		elif (time >= 21.0 and time < 22.0):
+			pedal = 1.0
+		time = time + 0.01
 
-    # ploting simulation data
-    _, ax = plt.subplots()
-    ax.plot(x_vals, height_vals, label="height (meters)")
-    ax.plot(x_vals, speed_vals, label="speed (m/s)")
-    ax.plot(x_vals, accel_vals, label="acceleration (m/s²)")
-    ax.set(xlabel='time (s)')
-    ax.grid()
-    plt.legend()
-    plt.show()
+	# ploting simulation data
+	_, ax = plt.subplots()
+	ax.plot(x_vals, height_vals, label="height (meters)")
+	ax.plot(x_vals, speed_vals, label="speed (m/s)")
+	ax.plot(x_vals, accel_vals, label="acceleration (m/s²)")
+	ax.set(xlabel='time (s)')
+	ax.grid()
+	plt.legend()
+	plt.show()
 
 if __name__ == "__main__":
-    demo()
+	demo()
